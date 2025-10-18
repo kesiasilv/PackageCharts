9387de05d0bcad8e1992fbd573991c179e695f26
Késia Silva <kesiasilvab6@gmail.com>
2025-10-18 16:54:39 -0300

    feat: Interacoes adicionada no gráfico

--- GraficosApp/GraficosApp/ContentView.swift
+++ GraficosApp/GraficosApp/ContentView.swift
@@ -9,6 +9,16 @@ import SwiftUI
 import Charts //importando a framework responsavel por criar a vizualizacao de graficos
 
 struct ContentView: View {
+    
+    @State private var rawSelectionDate: Date?//propriedade criada para usar nas interacoes
+    
+    var selectedViewMonth: ViewDado? {
+        guard let rawSelectionDate else { return nil }
+        return mockData.first{
+            Calendar.current.isDate(rawSelectionDate, equalTo: $0.date, toGranularity: .month)
+        }
+    }
+    
     var body: some View {
         VStack (alignment: .leading, spacing: 4){
             
@@ -22,7 +32,28 @@ struct ContentView: View {
             
             
             Chart{ //criado o grafico a partir dos dados da matriz que criamos
-                ForEach(viewDados) { viewDado in
+                
+                //criando um if para a UI das barras de quando selecionamos cada uma:
+                if let selectedViewMonth {
+                    RuleMark(x: .value("Selected Month", selectedViewMonth.date, unit: .month)) //aqui criei uma linha vertical que adiciona no eixo X se eu quisesse uma horizontal no eixo Y criava como tal.
+                        .foregroundStyle(.secondary.opacity(0.3))//personalizando a linha
+                    //criando a UI que ficara por cima da linha
+                        .annotation(position: .top, overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                            VStack{
+                                Text(selectedViewMonth.date, format: .dateTime.month(.wide))
+                                    .bold()
+                                
+                                Text("\(selectedViewMonth.viewCount)")
+                                .font(.title3.bold())
+                            }
+                            //personalizando o tamanho do quadro que exibirá quando segurar na barra
+                            .foregroundStyle(.white)
+                            .padding(12)
+                            .frame(width: 120)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.pink.gradient))
+                        }
+                }
+                ForEach(mockData) { viewDado in
                     //criadno uma vizualizacao de grafico do tipo de barras
                     BarMark(
                         x: .value("Ano", viewDado.date, unit: .month),
@@ -30,19 +61,30 @@ struct ContentView: View {
                         )
                     //criando uma personalizacao básica:
                     .foregroundStyle(Color.pink.gradient)
+                    //adicionando a opacidade quando clica na barra:
+                    .opacity(rawSelectionDate == nil || viewDado.date == selectedViewMonth? .date ? 1 : 0.5)
+                    
+                    
                 }//tudo isso atua como uma ZStack ou seja posso estabelecer as regras atras das barras do meu grafico
             }
             
             
             .frame(height: 180)  //definindo um tamanho para o gráfico
             
+            //criando o drang in drop que passa por cima de cada barra do grafico:
+            .chartXSelection(value: $rawSelectionDate.animation(.easeInOut)) //a animacao é bem minima para mudar a interface quando passa pelas barras mas sem a condicao de cima essa animação n funciona
+//            //para fins de testes e ver se ta funcionando o drang in drop
+//            .onChange(of: selectedViewMonth?.viewCount, { oldValue, newValue in
+//                print(newValue)
+//            })
+            
             .chartXAxis{ //personaliza o eixo X
-                AxisMarks(values: viewDados.map { $0.date }) { date in
+                AxisMarks(values: mockData.map { $0.date }) { date in
                     AxisGridLine() //adiciona linhas como uma grade
                     AxisValueLabel(format: .dateTime.month(.narrow), centered: true)
                 }
             }
-           
+    
             .chartYAxis{//personaliza o eixo Y
                 AxisMarks{ mark in
                     AxisValueLabel()
@@ -75,7 +117,7 @@ struct ViewDado: Identifiable {
 }
 
 //aqui criamos a vizualizacao dos daods oque sera exibido nesse caso um array de numeros por anos
-let viewDados: [ViewDado] = [
+let mockData: [ViewDado] = [
     .init(date: Date.from(year: 2025, month: 01, day: 01), viewCount: 1340),
     .init(date: Date.from(year: 2025, month: 02, day: 01), viewCount: 3245),
     .init(date: Date.from(year: 2025, month: 03, day: 01), viewCount: 6785),
