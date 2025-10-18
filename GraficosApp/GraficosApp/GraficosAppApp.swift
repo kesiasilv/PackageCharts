a9de00ae498f142bed7d6ca5bc80112cfec65683
Késia Silva <kesiasilvab6@gmail.com>
2025-10-18 14:38:58 -0300

    first commit

---
+++ GraficosApp/GraficosApp/GraficosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraficosAppApp.swift
+//  GraficosApp
+//
+//  Created by Késia Silva Viana on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GraficosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
