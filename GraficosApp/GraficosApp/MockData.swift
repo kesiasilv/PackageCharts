e698b37f44b10f146fa99f05f14274784860fa22
Késia Silva <kesiasilvab6@gmail.com>
2025-10-18 22:57:30 -0300

    feat: adicionei DonutsCharts e LineCharts..

--- GraficosApp/GraficosApp/MockData.swift
+++ GraficosApp/GraficosApp/MockData.swift
@@ -12,20 +12,21 @@ struct ViewDado: Identifiable {
     let id = UUID()
     let date: Date
     let viewCount: Int
+    let name: String //adicionado para testar o gráfico de pizza
     
     static let mockData: [ViewDado] = [
-        .init(date: Date.from(year: 2025, month: 01, day: 01), viewCount: 1340),
-        .init(date: Date.from(year: 2025, month: 02, day: 01), viewCount: 3245),
-        .init(date: Date.from(year: 2025, month: 03, day: 01), viewCount: 6785),
-        .init(date: Date.from(year: 2025, month: 04, day: 01), viewCount: 2889),
-        .init(date: Date.from(year: 2025, month: 05, day: 01), viewCount: 3472),
-        .init(date: Date.from(year: 2025, month: 06, day: 01), viewCount: 7773),
-        .init(date: Date.from(year: 2025, month: 07, day: 01), viewCount: 2203),
-        .init(date: Date.from(year: 2025, month: 08, day: 01), viewCount: 6646),
-        .init(date: Date.from(year: 2025, month: 09, day: 01), viewCount: 1030),
-        .init(date: Date.from(year: 2025, month: 10, day: 01), viewCount: 3256),
-        .init(date: Date.from(year: 2025, month: 11, day: 01), viewCount: 3321),
-        .init(date: Date.from(year: 2025, month: 12, day: 01), viewCount: 3321)
+        .init(date: Date.from(year: 2025, month: 01, day: 01), viewCount: 1340, name: "Jan"),
+        .init(date: Date.from(year: 2025, month: 02, day: 01), viewCount: 3245, name: "Fev"),
+        .init(date: Date.from(year: 2025, month: 03, day: 01), viewCount: 6785, name: "Mar"),
+        .init(date: Date.from(year: 2025, month: 04, day: 01), viewCount: 2889, name: "Abr"),
+        .init(date: Date.from(year: 2025, month: 05, day: 01), viewCount: 3472, name: "Mai"),
+        .init(date: Date.from(year: 2025, month: 06, day: 01), viewCount: 7773, name: "Jun"),
+        .init(date: Date.from(year: 2025, month: 07, day: 01), viewCount: 2203, name: "Jul"),
+        .init(date: Date.from(year: 2025, month: 08, day: 01), viewCount: 6646, name: "Ago"),
+        .init(date: Date.from(year: 2025, month: 09, day: 01), viewCount: 1030, name: "Set"),
+        .init(date: Date.from(year: 2025, month: 10, day: 01), viewCount: 3256, name: "Out"),
+        .init(date: Date.from(year: 2025, month: 11, day: 01), viewCount: 3321, name: "Nov"),
+        .init(date: Date.from(year: 2025, month: 12, day: 01), viewCount: 3321, name: "Dez")
     ]
 }
 
